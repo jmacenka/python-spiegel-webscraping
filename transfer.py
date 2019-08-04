749106bce4e093e0e8ee64e76e0f88668efc1b2f
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-08-04 23:13:12 +0200

    Changed errors in pathing in transfer-script

--- transfer.py
+++ transfer.py
@@ -2,9 +2,11 @@ import os
 from shutil import copyfile
 
 from_dir = '/var/lib/docker/volumes/webscraping/_data'
-to_dir = '/files'
+to_dir = 'files'
 
-to_dir_abs = os.path.abspath(to_dir)
+to_dir_abs = os.path.join(os.getcwd(),to_dir)
+
+print(to_dir_abs)
 
 if not os.path.exists(to_dir_abs):
     os.makedirs(to_dir_abs)
@@ -13,7 +15,8 @@ all_files = [f for f in os.listdir(from_dir) if 'html' in f]
 
 for f in all_files:
     from_path = os.path.join(from_dir,f)
-    copyfile(from_path,to_dir_abs)
+    to_path = os.path.join(to_dir_abs,f)
+    copyfile(from_path,to_path)
     print(f'Copied: {f}')
 
 print('\nAll Done')
